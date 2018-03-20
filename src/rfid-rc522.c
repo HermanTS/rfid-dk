7b8ad68e7349d9c164422e4f36a90d1506d47656
German Tsvetkov <gstsvetkov@spb.rpkb.ru>
2018-03-20 11:59:09 +0300

    Разметка функций для работы с RC522

---
+++ src/rfid-rc522.c
@@ -0,0 +1,33 @@
+/*
+ * rfid-rfid_rc522.c
+ *
+ *  Created on: 20 мар. 2018 г.
+ *      Author: gstsvetkov
+ */
+
+void rfid_rc522_activate(){
+
+}
+
+void rfid_rc522_deactivate(){
+
+}
+
+uint32_t rfid_rc522_read(){
+
+return 1;
+}
+
+void rfid_rc522_write(uint32_t data){
+
+}
+
+void rfid_rc522_gpio_init(){
+
+}
+
+void rfid_rc522_spi_init(){
+
+}
+
+
